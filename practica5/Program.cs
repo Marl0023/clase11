106a0573468b460d2cdf07a433466e58cd81ba5c
Marl0023 <marlotay2312@gmail.com>
2024-10-24 11:39:26 -0500

    SD

--- practica5/Program.cs
+++ practica5/Program.cs
@@ -5,10 +5,10 @@ foreach (string integrante in familia) {
 
     if (integrante == bnombre)
     {
-        Console.WriteLine("El nombre {0}que busca si se enceuntra en la lista ", integrante);
+        Console.WriteLine("El nombre {0} que busca si se enceuntra en la lista ", integrante);
         int index = Array.IndexOf(familia, bnombre);
         Console.WriteLine("El espacio que se encuentra en la casilla numero " + index);
         Environment.Exit(0);
     }
 }
-Console.WriteLine("Nombre encontrado");
+Console.WriteLine("Nombre no encontrado");
